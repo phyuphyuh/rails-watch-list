f5d4496678a257261d9f930480ff329b3686be50
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2024-12-21 23:41:59 +0630

    changed bunch of stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,23 @@
 require 'open-uri'
+require 'json'
 Movie.destroy_all
-# List.destroy_all
 
-# the Le Wagon copy of the API
-url = 'http://tmdb.lewagon.com/movie/top_rated'
-response = JSON.parse(URI.open(url).read)
+def api_key
+  ENV["MOVIE_API"]
+end
 
-response['results'].each do |movie_hash|
-  puts
-  p movie_hash
-  # create an instance with the hash
-  Movie.create!(
-    poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path'],
-    rating: movie_hash['vote_average'],
-    title: movie_hash['title'],
-    overview: movie_hash['overview']
-  )
+(1..15).to_a.each do |num|
+  api_data = { key: api_key }
+  # url = "http://tmdb.lewagon.com/movie/top_rated?page=#{num}"
+  url = "https://api.themoviedb.org/3/movie/top_rated?page=#{num}&api_key=#{api_data[:key]}"
+  response = JSON.parse(URI.open(url).read)
+
+  response['results'].each do |movie_hash|
+    Movie.create!(
+      title: movie_hash['title'],
+      poster_url: "https://image.tmdb.org/t/p/w500#{movie_hash['poster_path']}",
+      rating: movie_hash['vote_average'],
+      overview: movie_hash['overview']
+    )
+  end
 end
