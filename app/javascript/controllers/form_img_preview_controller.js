ae6a174eeeb4c0cc69bb6c3923a54219f2959be8
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2024-12-25 23:24:48 +0800

    new list form with image preview

---
+++ app/javascript/controllers/form_img_preview_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["input", "preview"];
+
+  preview() {
+    const file = this.inputTarget.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        let image = this.previewTarget.querySelector("img");
+        if (!image) {
+          image = document.createElement("img");
+          image.style.maxWidth = "240px";
+          this.previewTarget.appendChild(image);
+        }
+        // this.previewTarget.setAttribute('src', event.target.result);
+        image.src = event.target.result;
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+}
