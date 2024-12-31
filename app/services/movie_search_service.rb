7e1136ce699b763817c53a3fccd1e10d9bbc9514
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2025-01-01 00:11:54 +0800

    service file changes

--- app/services/movie_search_service.rb
+++ app/services/movie_search_service.rb
@@ -31,16 +31,27 @@ class MovieSearchService
       # results.filter_map do |result|
       #   next if result['id'].nil? || result['title'].nil?
       results.map do |result|
-        movie = Movie.find_or_initialize_by(api_id: result['id'])
-        if movie.new_record?
-          movie.title = result['title']
-          movie.poster_url = "https://image.tmdb.org/t/p/w500#{result['poster_path']}" if result['poster_path']
-          movie.rating = result['vote_average']
-          movie.overview = result['overview'] || "No overview available."
-          movie.save!
-          Rails.logger.debug "Created Movie: #{movie.title} (API ID: #{movie.api_id})"
+        # movie = Movie.find_or_initialize_by(api_id: result['id'])
+        # if movie.new_record?
+        #   movie.title = result['title']
+        #   movie.poster_url = "https://image.tmdb.org/t/p/w500#{result['poster_path']}" if result['poster_path']
+        #   movie.rating = result['vote_average']
+        #   movie.overview = result['overview'] || "No overview available."
+        #   movie.save!
+        #   Rails.logger.debug "Created Movie: #{movie.title} (API ID: #{movie.api_id})"
+        # end
+        # movie
+        movie = Movie.find_by(api_id: result['id'])
+        if movie.nil?
+          Movie.create(
+            title: result['title'],
+            poster_url: "https://image.tmdb.org/t/p/w500#{result['poster_path']}",
+            rating: result['vote_average'],
+            overview: result['overview'] || "No overview available."
+          )
+        else
+          movie
         end
-        movie
       end
     rescue OpenURI::HTTPError => e
       Rails.logger.error("Error fetching movie search: #{e.message}")
