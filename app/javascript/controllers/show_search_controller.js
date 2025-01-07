12db19df252fa77577c9850880874f3f4622cebc
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2025-01-07 20:08:42 +0630

    show page search styling

--- app/javascript/controllers/show_search_controller.js
+++ app/javascript/controllers/show_search_controller.js
@@ -1,16 +1,30 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["searchSection", "searchInput"]
+  static targets = ["searchSection", "searchInput", "addButton"]
 
   connect() {
     console.log("show search connected");
   }
 
-  displaySearch() {
-    this.searchSectionTarget.classList.toggle('d-none');
-    // this.searchInputTarget.classList.toggle('autofocus');
-    this.searchInputTarget.focus();
+  expand(event) {
+    event.currentTarget.classList.add("expanded");
+  }
 
+  shrink(event) {
+    event.currentTarget.classList.remove("expanded");
+  }
+
+  displaySearch(event) {
+    // this.searchSectionTarget.classList.remove('d-none');
+    this.searchSectionTarget.classList.add('active');
+    this.searchInputTarget.focus();
+    event.currentTarget.classList.add('d-none');
+  }
+
+  closeSearch() {
+    // this.searchSectionTarget.classList.add('d-none');
+    this.searchSectionTarget.classList.remove('active');
+    this.addButtonTarget.classList.remove('d-none');
   }
 }
