a4cb151f8c81a8bb918136c33fb719501f210eac
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2025-01-10 02:21:50 +0700

    bookmark details

---
+++ app/javascript/controllers/expand_card_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["card", "details"];
+  static values = { id: Number };
+
+  connect() {
+    this.expandedCard = null;
+  }
+
+  toggle(event) {
+    const clickedCard = event.currentTarget;
+
+    if (this.expandedCard) {
+      this.expandedCard.classList.remove("expanded");
+    }
+
+    if (this,this.expandedCard !== clickedCard) {
+      clickedCard.classList.add("expanded");
+      this.expandedCard = clickedCard;
+    } else {
+      this.expandedCard = null;
+    }
+  }
+
+}
