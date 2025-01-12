558b5e5f68d483ed78f0871126ef76add79429c7
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2025-01-12 23:03:59 +0700

    fixed edit comment

--- app/javascript/controllers/edit_comment_controller.js
+++ app/javascript/controllers/edit_comment_controller.js
@@ -1,17 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["comment", "card", "form", "input"]
+  static targets = ["comment", "form", "partial", "input"]
 
   connect() {
     console.log("comment connected");
-
-    this.element.addEventListener("turbo:frame-load", this.resetFormVisibility.bind(this));
-}
-
-  disconnect() {
-    // Clean up the event listener when the stimulus controller is disconnected
-    this.element.removeEventListener("turbo:frame-load", this.resetFormVisibility.bind(this));
   }
 
   displayForm(event) {
@@ -25,30 +18,23 @@ export default class extends Controller {
     event.stopPropagation();
   }
 
-  update(event) {
-    event.preventDefault();
+  // update(event) {
+  //   event.preventDefault();
 
-    const url = this.formTarget.action;
+  //   const url = this.formTarget.action;
 
-    fetch(url, {
-      method: 'PATCH',
-      // headers: { 'Accept': 'text/html' },
-      headers: { 'Accept': 'text/vnd.turbo-stream.html' },
-      body: new FormData(this.formTarget)
-    })
-    .then(response => response.text())
-    .then((html) => {
-      console.log(html);
-      // this.commentTarget.innerHTML = html;
-      // this.commentTarget.classList.remove('d-none');
-      // this.formTarget.classList.add('d-none');
-    })
-  }
-
-  resetFormVisibility() {
-    console.log("Resetting form visibility");
-    // Ensure the comment is visible, and the form is hidden
-    this.commentTarget.classList.remove('d-none');
-    this.formTarget.classList.add('d-none');
-  }
+  //   fetch(url, {
+  //     method: 'PATCH',
+  //     headers: { 'Accept': 'text/html' },
+  //     // headers: { 'Accept': 'text/vnd.turbo-stream.html' },
+  //     body: new FormData(this.formTarget)
+  //   })
+  //   .then(response => response.text())
+  //   .then((data) => {
+  //     console.log(data);
+  //     this.partialTarget.outerHTML = data;
+  //     this.commentTarget.classList.remove('d-none');
+  //     this.formTarget.classList.add('d-none');
+  //   })
+  // }
 }
